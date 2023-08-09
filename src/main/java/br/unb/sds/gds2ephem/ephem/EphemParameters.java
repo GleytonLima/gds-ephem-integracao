f693495e9aedfe5c3ef66e300f0e6a7cec4567d1
Gleyton Lima <gleytonclima@gmail.com>
2023-08-09 19:02:48 -0400

    adicionando status do sinal na saída

--- src/main/java/br/unb/sds/gds2ephem/ephem/EphemParameters.java
+++ src/main/java/br/unb/sds/gds2ephem/ephem/EphemParameters.java
@@ -14,7 +14,7 @@ public class EphemParameters {
     public static final String MAIL_MESSAGE_MODEL_NAME = "mail.message";
     public static final String EOC_SIGNAL_TAGS_MODEL_NAME = "eoc.signal.tags";
     public static final String SIGNAL_MODEL_NAME = "eoc.signal";
-    public static final List<String> SIGNAL_DEFAULT_PARAMETERS = asList("id", "description", "confidentiality", "tag_ids", "general_hazard_id", "specific_hazard_id", "country_id", "state_id", "district_ids", "signal_type", "report_date", "incident_date", "name", "message_ids");
+    public static final List<String> SIGNAL_DEFAULT_PARAMETERS = asList("id", "description", "confidentiality", "tag_ids", "general_hazard_id", "specific_hazard_id", "country_id", "state_id", "district_ids", "signal_type", "report_date", "incident_date", "name", "message_ids", "signal_stage_state_id");
 
     private String nomeModelo;
     private List<String> fields;
