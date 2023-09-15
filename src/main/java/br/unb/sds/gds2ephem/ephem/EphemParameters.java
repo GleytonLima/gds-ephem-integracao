d2cf54d2f8a8814d91f4cf544dbf8e25725bea98
Gleyton Lima <gleytonclima@gmail.com>
2023-09-14 22:38:32 -0400

    adicionando campos na saida dos sinais

--- src/main/java/br/unb/sds/gds2ephem/ephem/EphemParameters.java
+++ src/main/java/br/unb/sds/gds2ephem/ephem/EphemParameters.java
@@ -14,7 +14,49 @@ public class EphemParameters {
     public static final String MAIL_MESSAGE_MODEL_NAME = "mail.message";
     public static final String EOC_SIGNAL_TAGS_MODEL_NAME = "eoc.signal.tags";
     public static final String SIGNAL_MODEL_NAME = "eoc.signal";
-    public static final List<String> SIGNAL_DEFAULT_PARAMETERS = asList("id", "description", "confidentiality", "tag_ids", "general_hazard_id", "specific_hazard_id", "country_id", "state_id", "district_ids", "signal_type", "report_date", "incident_date", "name", "message_ids", "signal_stage_state_id");
+    public static final List<String> SIGNAL_DEFAULT_PARAMETERS = asList("id",
+            "description",
+            "confidentiality",
+            "tag_ids",
+            "general_hazard_id",
+            "specific_hazard_id",
+            "country_id",
+            "state_id",
+            "district_ids",
+            "signal_type",
+            "report_date",
+            "incident_date",
+            "name",
+            "outcome",
+            "outcome_justification_id",
+            "people_affected",
+            "message_ids",
+            "signal_stage_state_id",
+            "active",
+            "aetiology_id",
+            "animals_affected",
+            "create_date",
+            "date_closed",
+            "date_outcome_decided",
+            "description",
+            "display_name",
+            "is_event_closed",
+            "under_verification_date",
+            "verification",
+            "verification_source_id",
+            "verified_date",
+            "was_closed",
+            "was_closed_date",
+            "was_discarded",
+            "was_discarded_date",
+            "was_event",
+            "was_event_date",
+            "was_monitored",
+            "was_monitored_date",
+            "was_under_verification",
+            "was_under_verification_date",
+            "__last_update"
+            );
 
     private String nomeModelo;
     private String contextLang;
